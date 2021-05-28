444f853906ff0e5f8ede5905d7be5fcc5bb688af
unknown <jhermans58@gmail.com>
2021-05-28 17:00:04 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <Container>
+            <LogoContainer>
+                <img />
+            </LogoContainer>
+            <MenuContainer>
+                <a>
+                    <span>Home</span>
+                </a>
+                <a>
+                    <span>Home</span>
+                </a>
+                <a>
+                    <span>Home</span>
+                </a>
+                <a>
+                    <span>Home</span>
+                </a>
+                <a>
+                    <span>Home</span>
+                </a>
+            </MenuContainer>
+        </Container>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+
+`
+
+const LogoContainer = styled.div`
+
+`
+const MenuContainer = styled.div`
+
+`
